be6de755ccd72720a3c7126f7f148163456625b4
HHHans <martina-hans@web.de>
2022-01-20 12:11:10 +0100

    continued modular text with variables

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ texte = {
     'text_begruessung_neu': 'Servus!\nSchön, dass du die Experten für PKW, Motorrad, Wohnwagen und -mobil kontaktiert '
                        'hast! Mit unserem virtuellen WhatsApp Assistenten kannst du uns alle wichtigen Informationen '
                        'zu deinem Anliegen mitteilen. \n Willst du \n 1⃣ *fortfahren* oder \n 0⃣ *abbrechen?*',
+    'text_abbruch_nutzer': f'Der virtuelle Assistent wurde auf deinen Wunsch beendet - alle über dich gespeicherten '
+                           f'Daten wurden gelöscht. \n Du kannst uns auch eine Nachricht an <e-Mail> schicken oder '
+                           f'uns zu unseren Öffnungszeiten (9.00 - 18:00) telefonisch erreichen.\n Für mehr '
+                           f'Informationen besuche unsere Website {websiteurl}.',
     'text_ungueltige_auswahl': 'Bitte gib eine gültige Auswahl ein!',
     'text_anliegen_art': 'Was möchtest du? \n1⃣ eine *Wertermittlung* oder \n2⃣ eine *Schadenfeststellung* \ndeines '
                     'Fahrzeugs? \nOder willst du 0⃣ *abbrechen?* '
@@ -50,7 +54,7 @@ def reply():
             users.update_one(
                 {"number": number}, {"$set": {"status": "anliegen_art"}})
         elif option == 0:
-            res.message(f"Der virtuelle Assistent wurde auf deinen Wunsch beendet - alle über dich gespeicherten Daten wurden gelöscht. \n Du kannst uns auch eine Nachricht an <e-Mail> schicken oder uns zu unseren Öffnungszeiten (9.00 - 18:00) telefonisch erreichen.\n Für mehr Informationen besuche unsere Website {websiteurl}.")
+            res.message(texte['text_abbruch_nutzer'])
             users.delete_one({"number": number})
         else:
             res.message(texte['text_ungueltige_auswahl'])
@@ -64,7 +68,7 @@ def reply():
         if option == 0:
             users.update_one(
                 {"number": number}, {"$set": {"status": "main"}})
-            res.message(f"Der virtuelle Assistent wurde auf deinen Wunsch beendet - alle über dich gespeicherten Daten wurden gelöscht. \n Du kannst uns auch eine Nachricht an <e-Mail> schicken oder uns zu unseren Öffnungszeiten (9.00 - 18:00) telefonisch erreichen.\n Für mehr Informationen besuche unsere Website {websiteurl}.")
+            res.message(texte['text_abbruch_nutzer'])
             users.delete_one({"number": number})
         elif 1 <= option <= 2:
             res.message(f"Der virtuelle Assistent wurde wegen einer Programmbaustelle - alle über dich gespeicherten Daten wurden gelöscht. \n Du kannst uns auch eine Nachricht an <e-Mail> schicken oder uns zu unseren Öffnungszeiten (9.00 - 18:00) telefonisch erreichen.\n Für mehr Informationen besuche unsere Website {websiteurl}.")
