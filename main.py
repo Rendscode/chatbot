ccf1eb02642cd89e6c52ec7b37d4167ccb82c28d
HHHans <martina-hans@web.de>
2022-01-20 12:01:36 +0100

    modular text, database erase for test purposes

--- main.py
+++ main.py
@@ -17,37 +17,43 @@ orders = db["anfragen"]
 
 app = Flask(__name__)
 
+texte = {
+    'text_begruessung_neu': 'Servus!\nSchön, dass du die Experten für PKW, Motorrad, Wohnwagen und -mobil kontaktiert '
+                       'hast! Mit unserem virtuellen WhatsApp Assistenten kannst du uns alle wichtigen Informationen '
+                       'zu deinem Anliegen mitteilen. \n Willst du \n 1⃣ *fortfahren* oder \n 0⃣ *abbrechen?*',
+    'text_ungueltige_auswahl': 'Bitte gib eine gültige Auswahl ein!',
+    'text_anliegen_art': 'Was möchtest du? \n1⃣ eine *Wertermittlung* oder \n2⃣ eine *Schadenfeststellung* \ndeines '
+                    'Fahrzeugs? \nOder willst du 0⃣ *abbrechen?* '
+        }
+
 
 @app.route('/', methods=['GET', 'POST'])
 def reply():
+    if (users.find_one() == True):
+        users.delete_many()
     text = request.form.get("Body")
     number = request.form.get("From")
     number = number.replace("whatsapp:", "")[:-2]  #truncate last two digits to protect privacy for this demo
     res = MessagingResponse()
     user = users.find_one({"number": number})
     if bool(user) == False:
-        res.message("Servus!\nSchön, dass du die Experten für PKW, Motorrad, Wohnwagen und -mobil kontaktiert hast! Mit unserem virtuellen WhatsApp Assistenten kannst du uns alle wichtigen Informationen zu deinem Anliegen mitteilen. \n Willst du \n 1⃣ *fortfahren* oder \n 0⃣ *abbrechen?*")
-        users.insert_one({"number": number, "status": "main", "messages": []})
-    elif user["status"] == "main":
+        res.message(texte['text_begruessung_neu'])
+        users.insert_one({"number": number, "status": "start", "messages": []})
+    elif user["status"] == "start":
         try:
             option = int(text)
         except:
-            res.message("Bitte gib eine gültige Auswahl ein!")
+            res.message(texte['text_ungueltige_auswahl'])
             return str(res)
         if option == 1:
-            #res.message(f"Für mehr Informationen besuche unsere Website {websiteurl}.")
-            res.message("Was möchtest du? \n1⃣ eine *Wertermittlung* oder \n2⃣ eine *Schadenfeststellung* \ndeines Fahrzeugs? \nOder willst du 0⃣ *abbrechen?*")
+            res.message(texte['text_anliegen_art'])
             users.update_one(
                 {"number": number}, {"$set": {"status": "anliegen_art"}})
-            res.message(
-                "You can select one of the following cakes to order: \n\n1️⃣ Red Velvet  \n2️⃣ Dark Forest \n3️⃣ Ice Cream Cake"
-                "\n4️⃣ Plum Cake \n5️⃣ Sponge Cake \n6️⃣ Genoise Cake \n7️⃣ Angel Cake \n8️⃣ Carrot Cake \n9️⃣ Fruit Cake  \n0️⃣ Go Back")
         elif option == 0:
             res.message(f"Der virtuelle Assistent wurde auf deinen Wunsch beendet - alle über dich gespeicherten Daten wurden gelöscht. \n Du kannst uns auch eine Nachricht an <e-Mail> schicken oder uns zu unseren Öffnungszeiten (9.00 - 18:00) telefonisch erreichen.\n Für mehr Informationen besuche unsere Website {websiteurl}.")
             users.delete_one({"number": number})
         else:
-            res.message(
-                f"Bitte gibt eine gültige Auswahl ein!")
+            res.message(texte['text_ungueltige_auswahl'])
         return str(res)
     elif user["status"] == "anliegen_art":
         try:
@@ -67,27 +73,6 @@ def reply():
         res.message("Die letzte Eingabe kann nicht interpretiert werden.")
     users.update_one({"number": number}, {"$push": {"messages": {"text": text, "date": datetime.now()}}})
 
-
-
-    # msg = res.message(f"Danke für die Nachricht. Deine Nachricht '{text}' von der Nummer {number}")
-    # res.message("Willkommen beim virtuellen Assisteten von deinen Experten für PKW, Motorrad, Wohnwagen und -mobil!")
-    # if text == "Hi":
-    #     res.message("Holla")
-    # else:
-    #     res.message("Was soll man dazu sagen?")
-
-    # number = request.form.get('From')
-    # minute = 0
-    # # run loop for 10 minutes
-    #
-    # while True:
-    #     client.messages.create(body=f"{minute} minutes have passed since you've messaged me!", from_='whatsapp:+14155238886', to=number)
-    #     # Let's wait for 1 minute
-    #     time.sleep(60)
-    #     minute += 1
-    #     # If 10 minutes have passed then quit
-    #     if minute == 10:
-    #         break
     return str(res)
 
 if __name__ == '__main__':
